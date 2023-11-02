b582f5fe87cbf7f8005b4ec4df14ba0fe32e9d8b
Malejo01 <lizarragamauroalejandro@gmail.com>
2023-11-02 20:26:00 -0300

    quioscoapp

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import {PrismaClient} from '@prisma/client'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home({categorias}) {
+
+  console.log(categorias)
+
+  return (
+    <>
+    <h1>Hola marola</h1>
+    </>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient() 
+
+  const categorias = await prisma.categoria.findMany()
+  return {
+    props: {
+      categorias,
+    },
+  }
+
+  
+
+}
