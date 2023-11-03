1eaeef582d5cb62673756e203f87cffb1171e58e
Malejo01 <lizarragamauroalejandro@gmail.com>
2023-11-02 21:31:32 -0300

    context y hooks

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import {PrismaClient} from '@prisma/client'
+import Layout from "@/layout/layout"
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({categorias}) {
-
-  console.log(categorias)
+export default function Home() {
 
   return (
-    <>
+    <Layout>
     <h1>Hola marola</h1>
-    </>
+    </Layout>
   )
-}
-
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient() 
-
-  const categorias = await prisma.categoria.findMany()
-  return {
-    props: {
-      categorias,
-    },
-  }
-
-  
-
 }
