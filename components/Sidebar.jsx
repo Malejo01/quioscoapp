0458fe72cc24726953e4d9487840b317ad53657d
Malejo01 <lizarragamauroalejandro@gmail.com>
2023-11-15 15:11:42 -0300

    centrando logo empresa

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,13 +7,14 @@ const Sidebar = () => {
 
     return (
         <>
+            <div className="flex justify-center">
             <Image 
                 width={200} 
                 height={100} 
                 src="/assets/img/logo.svg" 
                 alt="Logo de la empresa"
             />       
-
+            </div>
             <nav className="mt-10">
                 {categorias.map(categoria => (
                     <Categoria
